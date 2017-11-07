17c2ccac5020ff3c8b275cf752e729a616ac95f8
Mary Clare Harrington <maryclareharrington@Marys-MacBook-Air.local>
2017-11-07 17:09:36 -0500

    second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,70 @@
 $(document).ready(function() {
-
-$(".available").on('click', function() {
+	
+	var ogText;
+	var name;
+	var tableNum;
+	var number;
+	var party;
+	var table;
+	var usersName;
+	var userNum;
+	var partySize;
+	
+	
+$(".available").on("click", function(event) {
+	table = $(this);
+	tableNum = table.text();
+	$("h4").text("Table Number: " + tableNum);
 	$(".form").css("display", "block");
-	$(this).removeClass('available').addClass('reserved');
 });
 	
-//	I kept trying various versions of the condition below but never got the class to switch back to ".available"
-//	if $(this.input.hasAttribute !== true) {
-//		$(this).removeClass('reserved').addClass('available');
-//	}
 	
  $(".available").mouseenter(function() {
     $(this).fadeTo(500, 0.5);
   });
+	
+	
   $(".available").mouseleave(function() {
     $(this).fadeTo(500, 1);
   });
+	
+	
+ $("div").mouseenter(function(e) {
+    if ($(this).hasClass("reserved")) {
+		$(".table").fadeToggle(500);
+		name = $(this).attr("name");
+		party = $(this).attr("party");
+		number = $(this).attr("number");
+		$(".tableName").text(name);
+		$(".tableNum").text(number);
+		$(".tableParty").text(party);
+		$(this).off("click");
+		$(".table").offset({left:e.pageX,top:e.pageY});
+	}
+  });
+	
+  $("div").mouseleave(function() {
+    if ($(this).hasClass("reserved")) {
+		$(".table").fadeToggle(500);
+	}
+  });
 
-$(".submit").on('click', function(){
-	$(".form").css("display", "none");
+$(".submit").on("click", function(){
+	usersName = $("#name-id").val();
+	$("#name-id").val("");
+	table.attr("name", usersName);
+	userNum = $("#num-id").val();
+	$("#num-id").val("");
+	table.attr("number", userNum);
+	partySize = $("#party-id").val();
+	$("#party-id").val("");
+	table.attr("party", partySize);
+	table.removeClass("available").addClass("reserved");
+	$(".form").hide();
 });
 
-$(".closeForm").on('click', function() {
+$(".closeForm").on("click", function() {
 	$(".form").css("display", "none");
-	$(".available").removeClass('reserve').addClass('available');
 });
 	
 
